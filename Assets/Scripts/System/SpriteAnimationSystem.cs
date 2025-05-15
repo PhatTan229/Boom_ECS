b2a65388a8211160179cc5c0e9de99b86e274879
Phat Tan <phattan229@gmail.com>
2025-05-15 11:14:25 +0700

    refactor sprite animtion system to job chunk

--- Assets/Scripts/System/SpriteAnimationSystem.cs
+++ Assets/Scripts/System/SpriteAnimationSystem.cs
@@ -1,15 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Burst.Intrinsics;
+using Unity.Collections;
 using Unity.Entities;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public partial struct SpriteAnimationSystem : ISystem
+[BurstCompile]
+public partial struct SpriteAnimationSystem : ISystem, ISystemStartStop
 {
-    public void OnUpdate(ref SystemState state) 
+    [BurstCompile]
+    partial struct AnimaitonUpdateJob : IJobChunk
     {
-        foreach (var (update, animation) in SystemAPI.Query<RefRW<SpriteAnimationUpdate>, RefRO<SpriteAnimation>>())
+        [NativeDisableParallelForRestriction] public ComponentTypeHandle<SpriteAnimationUpdate> updateHandler;
+        [ReadOnly] public ComponentTypeHandle<SpriteAnimation> animtionHandler;
+
+        public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask, in v128 chunkEnabledMask)
         {
-            update.ValueRW.Value = (float)animation.ValueRO.index;
+            var update = chunk.GetNativeArray(ref updateHandler);
+            var animation = chunk.GetNativeArray(ref animtionHandler);
+
+            for (var i = 0; i < chunk.Count; i++) 
+            {
+                var element = update[i];
+                element.Value = (float)animation[i].index;
+                update[i] = element;
+            }
         }
+    }
+
+    private EntityQuery query;
+
+    public void OnStartRunning(ref SystemState state)
+    {
+        query = state.GetEntityQuery(ComponentType.ReadWrite<SpriteAnimationUpdate>(), ComponentType.ReadOnly<SpriteAnimation>());
+    }
+
+    public void OnUpdate(ref SystemState state)
+    {
+        var job = new AnimaitonUpdateJob()
+        {
+            updateHandler = state.GetComponentTypeHandle<SpriteAnimationUpdate>(),
+            animtionHandler =state.GetComponentTypeHandle<SpriteAnimation>(true),
+        };
+        state.Dependency = job.ScheduleParallel(query, state.Dependency);
+    }
+
+    public void OnStopRunning(ref SystemState state)
+    {
     }
 }
